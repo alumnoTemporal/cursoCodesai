9140192916b2e5fac029a12e068be8e4181522c8
jonay <casa@gmail.com>
2016-10-09 04:11:00 +0100

    all prapared

---
+++ Test/testprueba.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Usuario on 09/10/2016.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class testprueba {
+
+
+    @Test
+    public void laPrueba(){
+        assertTrue(true);
+    }
+
+}
